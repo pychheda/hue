627094d6f3e4096c475cbdfed24d8b9e5dd3d6c9
Priyanka Chheda <priyanka.chheda@yahoo.com>
2021-02-19 03:31:33 +0530

    Revert "Added a grep command to skip CI checks"
    This reverts commit 85ae5c63f6081147acda2fd522e2fa0d180afae6.

--- tools/ci/check_for_python_lint.sh
+++ tools/ci/check_for_python_lint.sh
@@ -25,7 +25,6 @@ files=`git diff --name-only origin/master --diff-filter=b | egrep .py$ | \
   grep -v wsgiserver.py | \
   grep -v /migrations/ | \
   grep -v apps/oozie/src/oozie/tests.py | \
-  grep -v /tools/ops/ | \
   grep -v /org_migrations/`
 
 cd $HOME
